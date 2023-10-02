9ba57b2fd4d3b1ffcbcad4221be06b83719d4372
patrikrovnak <patrik.rovnak@inalogy.com>
2023-10-02 08:29:39 +0200

    added icfs connid constants

--- src/main/java/com/inalogy/midpoint/connectors/utils/Constants.java
+++ src/main/java/com/inalogy/midpoint/connectors/utils/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
 
     public static final String ISO_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String TIME_ZONE = "UTC";
-
+    public static final String ICFS_NAME = "__NAME__";
+    public static final String ICFS_UID = "__UID__";
+    public static final String ICFS_PASSWORD = "__PASSWORD__";
+    public static final String ICFS_ACTIVATION = "__ACTIVATION__";
     public static final String MONGODB_UID = "_id";
 }
